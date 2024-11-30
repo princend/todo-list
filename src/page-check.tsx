55490d0e91aa6c529d7ceec70cfe2c3d37ce1d53
princend <waxdji3g4y3c04@gmail.com>
2024-11-30 21:02:07 +0800

    更改todo樣式 使todo易點擊

--- src/page-check.tsx
+++ src/page-check.tsx
@@ -22,25 +22,29 @@ const PageCheck: React.FC<PageCheckProps> = ({ todos, toggleTodo, resetTodos }) 
       <button className="btn btn-warning mb-3" onClick={resetTodos}>
         重置勾選
       </button>
-      <ul className="list-group">
-        {todos.map(todo => (
-          <li
-            key={todo.id}
-            className="list-group-item d-flex justify-content-between align-items-center"
-            onClick={() => toggleTodo(todo.id)}  // 點擊整個項目切換勾選
-            style={{ cursor: 'pointer' }}  // 添加手形游標
-          >
-            <input
-              type="checkbox"
-              className="todo-checkbox"
-              checked={todo.completed}
-              onChange={() => {}}
-              readOnly  // 禁止直接修改勾選框，只有點擊項目才會改變
-            />
-            <span className="ms-3">{todo.text}</span>
-          </li>
-        ))}
-      </ul>
+
+
+      <div className="container">
+        <ul className="list-group">
+          {todos.map(todo => (
+            <li
+              key={todo.id}
+              className="list-group-item d-flex justify-content-between align-items-center"
+              onClick={() => toggleTodo(todo.id)}  // 點擊整個項目切換勾選
+              style={{ cursor: 'pointer' }}  // 添加手形游標
+            >
+              <input
+                type="checkbox"
+                className="todo-checkbox"
+                checked={todo.completed}
+                onChange={() => { }}
+                readOnly  // 禁止直接修改勾選框，只有點擊項目才會改變
+              />
+              <span className="ms-3">{todo.text}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
