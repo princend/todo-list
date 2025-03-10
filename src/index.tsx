d85e909c68dd41aa81c54174d425273dde7dd8e9
Princend <waxdji3g4y3c04@gmail.com>
2025-03-10 23:25:05 +0800

    ReactDOM改成createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from "react-dom/client";
 
-
-const root = ReactDOM.createRoot(
+const root = createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
